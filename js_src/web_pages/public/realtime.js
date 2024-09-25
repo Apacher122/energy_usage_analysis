cad55c4bde103b2167ecdb956bf25945f118817c
rcda <rcda.tech@gmail.com>
2024-09-25 11:36:02 -0500

    making linter happy

--- js_src/web_pages/public/realtime.js
+++ js_src/web_pages/public/realtime.js
@@ -1,12 +1,9 @@
+// eslint-disable-next-line no-undef
 const socket = io();
-console.log("TEST")
-const today = new Date();
-var datetime = today.toLocaleString();
 
 socket.on("INIT", (response) => {
     const data = JSON.parse(response)
     var table = document.getElementById('device_data')
-    var console = document.getElementById('console')
     var power_table = document.getElementById('current_wattage')
     for (const idx in data) {
         var row = `<tr class="device" id="item-id-${data[idx].entity_id}">
@@ -23,11 +20,8 @@ socket.on("INIT", (response) => {
 socket.on("UPDATE", (response) => {
     const data = JSON.parse(response)
     const today = new Date();
-    var datetime = today.toLocaleString();
-    var table = document.getElementById('device_data');
     var console = document.getElementById('console');
     var date = document.getElementById('date');
-    var power_table = document.getElementById('current_wattage')
 
     date.innerText = today;
     for (const idx in data) {
@@ -47,19 +41,6 @@ socket.on("UPDATE", (response) => {
         } else {
             wat_string = `${wattage} W`
         }
-        var row = `<tr class="item-id-${data[idx].entity_id}">
-                        <td>${data[idx].friendly_name}</td>
-                        <td>${kwh}</td>
-                        <td>${historical}</td>
-                        <td>${cost}</td>
-                        <td>${overall_cost}</td>
-                    </tr>`;
-
-        var row_2 = `<tr class="item-id-power-${data[idx].entity_id}">
-                        <td>${data[idx].friendly_name}</td>
-                        <td>${power_status}</td>
-                        <td>${wat_string}</td>
-                    </tr>`;
 
         try {
             var dev = document.getElementById("item-id-" + data[idx].entity_id)
